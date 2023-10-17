56efd6e310b1d0117b34991eaf20218a3f9f6dd9
patil-pus <patil.pus@northeastern.edu>
2023-10-17 18:59:25 -0400

    Merger

--- Packer/update_and_start_services.sh
+++ Packer/update_and_start_services.sh
@@ -1,10 +1,7 @@
 sudo DEBIAN_FRONTEND=noninteractive apt update -q
-
 sudo DEBIAN_FRONTEND=noninteractive apt -q --assume-yes install mariadb-client mariadb-server
 
 sudo apt update
-
-sudo apt --assume-yes install mariadb-client mariadb-server
 
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
