df369008bc031ed0b422656d4db9747a30e0b1d1
patil-pus <patil.pus@northeastern.edu>
2023-10-18 23:20:18 -0400

    merger

--- Packer/update_and_start_services.sh
+++ Packer/update_and_start_services.sh
@@ -1,9 +1,30 @@
+
 sudo DEBIAN_FRONTEND=noninteractive apt update -q
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -q -y
+
+sudo apt-get install -y nodejs npm
+
+sudo apt-get install -y curl software-properties-common gnupg
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+
+
+
+node -v
+npm -v
+
 sudo DEBIAN_FRONTEND=noninteractive apt -q --assume-yes install mariadb-client mariadb-server
-
-sudo apt update
-
+sudo mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH PRIVILEGES;"
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 
+sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip
+
+sudo unzip 'webapp.zip '-d webapp
 sudo apt-get clean
+
+
+
